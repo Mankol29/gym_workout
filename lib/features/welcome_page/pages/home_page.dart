654de01c25e1021dc6a44516f208f455b5d21223
Patryk Kromski <kromski29@interia.pl>
2023-01-24 22:42:55 +0100

    Add gym2 photo and start to doing home page

--- lib/features/welcome_page/pages/home_page.dart
+++ lib/features/welcome_page/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:gym_workout/features/welcome_page/welcome_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,7 +13,54 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey[800],
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const [
+            DrawerHeader(
+              child: Text('Navigation Drawer'),
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.green[700],
+        centerTitle: true,
+        title: const Text('Your training'),
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              'images/gym-2.jpg',
+            ),
+            opacity: 50,
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.07,
+                color: Color.fromARGB(160, 55, 116, 57),
+                child: Center(
+                  child: Text(
+                    'Add or Sellect your own plan for today',
+                    style: GoogleFonts.bebasNeue(
+                        textStyle: TextStyle(color: Colors.white),
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
