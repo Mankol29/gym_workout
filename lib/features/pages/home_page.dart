4ba724014d9aa92848ce82fa39b02aada5ea4c73
Patryk Kromski <kromski29@interia.pl>
2023-02-20 22:56:39 +0100

    add functionality of add workout and created WorkoutPage

--- lib/features/pages/home_page.dart
+++ lib/features/pages/home_page.dart
@@ -3,6 +3,8 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:gym_workout/features/data/workout_data.dart';
 import 'package:provider/provider.dart';
 
+import 'workout_page.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -11,14 +13,65 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // text controller
+  final newWorkoutSaveController = TextEditingController();
+
   void createNewWorkout() {
     showDialog(
         context: context,
-        builder: (context) => const AlertDialog(
-              title: Text("Create new workout"),
-              content: TextField(),
-              actions: [],
+        builder: (context) => AlertDialog(
+              title: const Text("Create new workout"),
+              content: TextField(
+                controller: newWorkoutSaveController,
+              ),
+              actions: [
+                // save button
+                MaterialButton(
+                  onPressed: save,
+                  child: const Text('Save'),
+                ),
+
+                // cancel button
+
+                MaterialButton(
+                  onPressed: cancel,
+                  child: const Text('Cancel'),
+                )
+              ],
             ));
+  }
+
+  // go to workout page
+  void workoutPage(String workoutName) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => WorkoutPage(workoutName: workoutName),
+        ));
+  }
+
+  // save workout
+  void save() {
+    // get workout name from controller
+    String newWorkoutSave = newWorkoutSaveController.text;
+    //add workout
+    Provider.of<WorkoutPlan>(context, listen: false).addWorkout(newWorkoutSave);
+
+    // pop of dialog
+    Navigator.pop(context);
+    clear();
+  }
+
+  // cancel workout
+  void cancel() {
+    // pop of dioalog
+    Navigator.pop(context);
+    clear();
+  }
+
+  // clear dialog
+  void clear() {
+    newWorkoutSaveController.clear();
   }
 
   @override
@@ -67,10 +120,25 @@ class _HomePageState extends State<HomePage> {
                   scrollDirection: Axis.vertical,
                   itemCount: value.getWorkoutList().length,
                   itemBuilder: (context, index) => ListTile(
-                    title: Text(
-                      value.getWorkoutList()[index].name,
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold),
+                    title: Container(
+                      padding: EdgeInsets.all(16),
+                      color: Colors.white54,
+                      child: Text(
+                        value.getWorkoutList()[index].name,
+                        style: const TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    trailing: Container(
+                      color: Colors.white54,
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.black,
+                        ),
+                        onPressed: () =>
+                            workoutPage(value.getWorkoutList()[index].name),
+                      ),
                     ),
                   ),
                 ),
